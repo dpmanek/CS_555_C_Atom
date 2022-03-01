8919dcaeeb5da281205f45232ba92f14c80c34af
dpmanek <dpmanek@gmail.com>
2022-03-01 15:30:59 -0500

    Adding Test for avatar.py

--- avatar.py
+++ avatar.py
@@ -7,7 +7,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Shooter')
+pygame.display.set_caption('Monkey Avatar')
 
 
 class Soldier(pygame.sprite.Sprite):
