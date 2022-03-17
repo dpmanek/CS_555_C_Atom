ff800c9e93b4661cc5279e273b39fdc67ede86a1
Surya N <49335157+Surya7612@users.noreply.github.com>
2022-03-17 15:14:13 -0400

    This file includes Avatar controls, collision to the floor, and sprite animations

---
+++ Ms.Kong.py
@@ -0,0 +1,186 @@
+import pygame
+import os
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Mrs.Kong')
+
+#setting framerate 
+clock = pygame.time.Clock()
+FPS = 60
+
+#defining game variable
+GRAVITY = 0.75
+
+#defining player action variables
+moving_left = False
+moving_right = False
+
+#define colors
+BG = (144,201,120)
+RED = (255,0,0)
+
+def draw_bg():
+    screen.fill(BG)
+    pygame.draw.line(screen, RED, (0,300), (SCREEN_WIDTH, 300))
+
+
+class Kong(pygame.sprite.Sprite):
+    def __init__(self, char_type, x, y, scale, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = False
+        self.flip = False
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+
+
+        #loading all images for the players
+        animation_types = ['Idle', 'Run', 'Jump']
+        for animation in animation_types:
+            #reseting temporary list of images
+            temp_list = []
+            #counting number of files in the folder
+            num_of_frames = len(os.listdir(f'/Users/surya/Desktop/img/{self.char_type}/{animation}'))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f'/Users/surya/Desktop/img/{self.char_type}/{animation}/{i}.png')
+                img = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale)))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+
+    def move(self, moving_left, moving_right):
+        #reset movement variables
+        dx = 0
+        dy = 0
+
+        #assign movement variables if moving left or right
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        #jump
+        if self.jump == True and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        #applying gravity
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+          self.vel_y
+        dy += self.vel_y
+
+        #checking collision with floor
+        if self.rect.bottom +dy >300:
+            dy = 300 - self.rect.bottom
+            self.in_air = False
+
+        #updating rectangular position
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def update_animation(self):
+        #updating animation
+        ANIMATION_COOLDOWN = 100
+        #updating images dependent on current frame
+        self.image = self.animation_list[self.action][self.frame_index]
+        #check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        #if the animation has run out the reset back to start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    def update_action(self, new_action):
+        #check if the new action is different to the previous one
+        if new_action!=self.action:
+            self.action = new_action
+            #updating the animation setttings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+player = Kong('player', 200, 200, 3, 5)
+enemy = Kong('enemy', 400, 200, 3, 5)
+
+
+
+run = True
+while run:
+
+    clock.tick(FPS)
+
+    draw_bg()
+
+    player.update_animation()
+    player.draw()
+    enemy.draw()
+
+
+    #update player actions
+    if player.alive:
+        if player.in_air:
+            player.update_action(2)#2: jump
+        elif moving_left or moving_right:
+            player.update_action(1)#1: run
+        else:
+            player.update_action(0)#0: idle
+        player.move(moving_left, moving_right)
+
+
+    for event in pygame.event.get():
+        #quit game
+        if event.type == pygame.QUIT:
+            run = False
+        #keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+
+        #keyboard button released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+
+
+
+
+    pygame.display.update()
+
+pygame.quit()
+
+    
+     
+
