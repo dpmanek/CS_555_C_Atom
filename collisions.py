5dfdc83053189138b6e0373c42ed05440aaa53ad
dpmanek <dpmanek@gmail.com>
2022-03-21 17:51:20 -0400

    animating obstacles

---
+++ collisions.py
@@ -0,0 +1,40 @@
+
+
+class Kong(pygame.sprite.Sprite):
+#applying movements to the obstacles
+    def move(self, moving_left, moving_right):
+        #reset movement variables
+        dx = 0
+        dy = 0
+
+        #assign movement variables if moving left or right
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        #jump
+        if self.jump == True and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        #apply gravity
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+
+        #check collision with floor
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+            self.in_air = False
+
+        #update rectangle position
+        self.rect.x += dx
+        self.rect.y += dy
+
